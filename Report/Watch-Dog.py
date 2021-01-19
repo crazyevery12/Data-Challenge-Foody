1e93144679fb59cca4674a92904bcc0e53b53327
eric <anh.tran160201@vnuk.edu.vn>
2021-01-19 10:53:03 +0700

    Add to_sql funcion

--- Report/Watch-Dog.py
+++ Report/Watch-Dog.py
@@ -3,6 +3,8 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import papermill as pm
 import subprocess
+import pyodbc
+import pandas as pd
 
 
 class Watcher:
@@ -37,10 +39,11 @@ class Handler(FileSystemEventHandler):
             # Take any action here when a file is first created.
             print("Received created event - %s." % event.src_path)
             path = event.src_path
-            filename = path.split('\\')[-1]
-            if filename.endswith('.csv'):
+            file_name = path.split('\\')[-1]
+            if file_name.endswith('.csv'):
                 time.sleep(1)
-                generate_report(filename)
+                generate_report(file_name)
+                to_sql(file_name)
         elif event.event_type == 'modified':
             # Taken any action here when a file is modified.
             print("Received modified event - %s." % event.src_path)
@@ -74,6 +77,38 @@ def generate_report(csv_file):
     generate_pdf(notebook_name)
     pdf_report_name = f"{clean_name}.pdf"
     print(pdf_report_name)
+
+
+def to_sql(csv_file):
+    # Import CSV
+    data = pd.read_csv (csv_file)
+    df = pd.DataFrame(data, columns= ['Name','Position','Price','Quality','Service','Space','ZAvg_Score'])
+
+    # Connect to SQL Server
+    conn = pyodbc.connect('Driver={SQL Server};'
+                          'Server=DESKTOP-718QBB9\SQLEXPRESS;'
+                          'Database=FoodyDB;'
+                          'Trusted_Connection=yes;')
+    cursor = conn.cursor()
+
+    # Create Table
+    cursor.execute('CREATE TABLE foody_info (Name nvarchar(50),Position nvarchar(50), Price int(2), Quality int(2),Service int(2), Space int(2), ZAvg_Score int(2) )')
+
+    # Insert DataFrame to Table
+    for row in df.itertuples():
+        cursor.execute('''
+                    INSERT INTO TestDB.dbo.people_info (Name,Position,Price,Quality,Service,Space,ZAvg_Score)
+                    VALUES (?,?,?)
+                    ''',
+                    row.Name,
+                    row.Position,
+                    row.Price,
+                    row.Quality,
+                    row.Service,
+                    row.Space,
+                    row.ZAvg_Score
+                    )
+    conn.commit()
 
 
 if __name__ == '__main__':
